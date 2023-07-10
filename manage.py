25b34278c8779b2a9c0885c0c244d2c8bb615c15
Pavlo Shcherbukha <pavlo.shcherbukha@pravex.ua>
2023-07-10 13:19:36 +0300

    www

--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
 import sys
 import os
 import redis
-from rq import  Worker , Queue, Connection
+import rq
+from rq import Queue, Worker, Connection
+
 irds_host = os.getenv('RDS_HOST');
 irds_port = os.getenv('RDS_PORT');
 irds_psw = os.getenv('RDS_PSW');
@@ -11,12 +13,16 @@ def test():
     print("test command")
     print("======================")
 def run_worker():
-    red = redis.StrictRedis(irds_host, irds_port, charset="utf-8", password=irds_psw, decode_responses=True)
+    red = redis.StrictRedis(irds_host, irds_port, charset="utf-8", password=irds_psw, decode_responses=False)
     listen = [irdsq_outmsg]
     queue = Queue(irdsq_outmsg, connection=red)
     with Connection(red):
         worker = Worker(map(Queue, listen))
-        worker.work()
+        try:
+            worker.work(logging_level="DEBUG")
+        except Exception as e:
+            print(e)   
+            
         print('OK')
         print(worker.name)
         print('Successful jobs: ' + str(worker.successful_job_count))
